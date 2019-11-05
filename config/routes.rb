72f756ed488c60b4d800cbfed9c728a8aa3b5fb3
CeliaTHP <celia.95190@gmail.com>
2019-11-05 19:39:53 +0100

    views inc

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'static_pages/index'
   get 'static_pages/secret'
-  resources :events
+  get 'events/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
